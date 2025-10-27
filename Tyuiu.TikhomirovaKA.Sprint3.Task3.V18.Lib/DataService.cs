6f9cad333dd8a73fb34a77e5a3f03e9ed4e1de9d
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-10-27 15:03:33 +0500

    Task3

--- Tyuiu.TikhomirovaKA.Sprint3.Task3.V18.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint3.Task3.V18.Lib/DataService.cs
@@ -1,7 +1,19 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint3.Task3.V18.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
 
+namespace Tyuiu.TikhomirovaKA.Sprint3.Task3.V18.Lib
+{
+    public class DataService : ISprint3Task3V18
+    {
+        public string ReplaceNumOnChar(string value, char item)
+        {                  
+            foreach (char n in value)
+            {
+                if (Char.IsDigit(n))
+                {
+                    value = value.Replace(n, item);
+                }
+            }
+            return value;
+        }
     }
 }
