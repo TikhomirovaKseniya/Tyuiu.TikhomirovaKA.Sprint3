0e4b556a2987f2ab17120635c21fbd47326e4456
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-10-27 16:24:58 +0500

    Task7

--- Tyuiu.TikhomirovaKA.Sprint3.Task7.V22.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint3.Task7.V22.Lib/DataService.cs
@@ -1,7 +1,19 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint3.Task7.V22.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
 
+namespace Tyuiu.TikhomirovaKA.Sprint3.Task7.V22.Lib
+{
+    public class DataService : ISprint3Task7V22
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int length = (stopValue - startValue) + 1;
+            double[] results = new double[length];
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                double fx = Math.Sin(i) + ((Math.Cos(2 * i))/ 2) - 1.5 * i;
+                if (fx == 0) break; else results[i - startValue] = Math.Round(fx, 2);
+            }   
+            return results;
+        }
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint3.Task7.V22/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint3.Task7.V22/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TikhomirovaKA.Sprint3.Task7.V22.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Спринт #3                                                              *");
+        Console.WriteLine("* Тема: Алгоритмы циклической структуры                                  *");
+        Console.WriteLine("* Задание #7                                                             *");
+        Console.WriteLine("* Вариант #22                                                            *");
+        Console.WriteLine("* Выполнила: Выполнила Тихомирова К. А. | ИБКСб-25-1                     *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+        int startValue = -5;
+        Console.WriteLine(startValue);
+        int stopValue = 5;
+        Console.WriteLine(stopValue);
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+        double[] values = ds.GetMassFunction(startValue, stopValue);
+        Console.WriteLine(string.Join(", ", values));
+        Console.ReadKey();
+    }
+}
