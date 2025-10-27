b8d9345f7554efa9ec4ee90ff30f33f12c804228
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-10-27 15:48:14 +0500

    Task6

--- Tyuiu.TikhomirovaKA.Sprint3.Task6.V20.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint3.Task6.V20.Lib/DataService.cs
@@ -1,7 +1,23 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint3.Task6.V20.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
 
+namespace Tyuiu.TikhomirovaKA.Sprint3.Task6.V20.Lib
+{
+    public class DataService : ISprint3Task6V20
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                for (int d = 1; d < 12; d++)
+                {
+                    if (i % d == 0)
+                    {
+                        count++;
+                    }
+                }                
+            }
+            return count;
+        }
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint3.Task6.V20/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint3.Task6.V20/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TikhomirovaKA.Sprint3.Task6.V20.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Спринт #3                                                              *");
+        Console.WriteLine("* Тема: Алгоритмы циклической структуры                                  *");
+        Console.WriteLine("* Задание #6                                                             *");
+        Console.WriteLine("* Вариант #20                                                            *");
+        Console.WriteLine("* Выполнила: Выполнила Тихомирова К. А. | ИБКСб-25-1                     *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+        int startValue = 20;
+        Console.WriteLine(startValue);
+        int stopValue = 32;
+        Console.WriteLine(stopValue);
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+
+        Console.WriteLine(ds.GetSumTheDivisors(startValue, stopValue));
+        Console.ReadKey();
+    }
+}
