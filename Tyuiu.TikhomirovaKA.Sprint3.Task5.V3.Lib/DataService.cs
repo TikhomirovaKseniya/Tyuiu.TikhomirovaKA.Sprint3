b8e88e0c534229bd4d02b8d60bf269fdf89df8bc
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-10-27 15:35:18 +0500

    Task5

--- Tyuiu.TikhomirovaKA.Sprint3.Task5.V3.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint3.Task5.V3.Lib/DataService.cs
@@ -1,7 +1,20 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint3.Task5.V3.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
 
+namespace Tyuiu.TikhomirovaKA.Sprint3.Task5.V3.Lib
+{
+    public class DataService : ISprint3Task5V3
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {            
+            double result = 0;
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int j = startValue2; j <= stopValue2; j++)
+                {
+                    result += (Math.Sin(startValue2) + x) / x;
+                }
+            }
+            return Math.Round(result, 3);
+        }
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint3.Task5.V3/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint3.Task5.V3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TikhomirovaKA.Sprint3.Task5.V3.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Спринт #3                                                              *");
+        Console.WriteLine("* Тема: Алгоритмы циклической структуры                                  *");
+        Console.WriteLine("* Задание #5                                                             *");
+        Console.WriteLine("* Вариант #3                                                            *");
+        Console.WriteLine("* Выполнила: Выполнила Тихомирова К. А. | ИБКСб-25-1                     *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+        int x = 5;
+        Console.WriteLine("x = " + x);
+        int startValue1 = 1;
+        int startValue2 = 1;
+        int stopValue1 = 3;
+        int stopValue2 = 11;
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+
+        Console.WriteLine(ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+        Console.ReadKey();
+    }
+}
