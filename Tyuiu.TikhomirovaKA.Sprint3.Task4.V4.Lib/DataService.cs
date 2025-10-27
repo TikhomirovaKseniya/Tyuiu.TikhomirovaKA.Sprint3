b1b534038504a1546c05f226d6453f5c0e2a45f9
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-10-27 15:24:06 +0500

    Task4

--- Tyuiu.TikhomirovaKA.Sprint3.Task4.V4.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint3.Task4.V4.Lib/DataService.cs
@@ -1,7 +1,21 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint3.Task4.V4.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
 
+namespace Tyuiu.TikhomirovaKA.Sprint3.Task4.V4.Lib
+{
+    public class DataService : ISprint3Task4V4
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            int x;
+            double y = 0;
+            int i = 0;
+            for (x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0) break;
+                y += x/(Math.Cos(x) + Math.Sin(x));
+                i++;
+            }
+            return Math.Round(y, 3);
+        }
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint3.Task4.V4/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint3.Task4.V4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TikhomirovaKA.Sprint3.Task4.V4.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Спринт #3                                                              *");
+        Console.WriteLine("* Тема: Алгоритмы циклической структуры                                  *");
+        Console.WriteLine("* Задание #4                                                             *");
+        Console.WriteLine("* Вариант #4                                                            *");
+        Console.WriteLine("* Выполнила: Выполнила Тихомирова К. А. | ИБКСб-25-1                     *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+        int startValue = -5;
+        Console.WriteLine(startValue);
+        int stopValue = 5;
+        Console.WriteLine(stopValue);
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+
+        Console.WriteLine(ds.Calculate(startValue, stopValue));
+        Console.ReadKey();
+    }
+}
